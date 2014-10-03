0e56ffcd62bbfa6c4996b3e1a08095ff34ac7ffa
Andur Carr <carr.andur@gmail.com>
2014-10-02 21:42:19 -0700

    Fix seeding bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-scalar = 5
+SCALAR = 5
 
 #generate products
-6*scalar.times do
+(6*SCALAR).times do
 end
 
-#generate users, 100 users
-20*scalar.times do
-  sample_name = Faker::Name.name
-  u = User.new({name: sample_name, email: Faker::Internet.email(sample_name), phone_number: Faker::PhoneNumber.phone_number})
-  u.save
-end
-
-#generate addresses, scalar+scalar^2+scalar^3 cities
+#generate addresses, SCALAR+SCALAR^2+SCALAR^3 cities
 def sample_city
   {"city" => Faker::Address.city, "state" => Faker::Address.state, "zip" => (Faker::Address.zip_code).to_i}
 end
 
 def sample_cities
   cities, towns, villages = [], [], []
-  scalar.times do
+  SCALAR.times do
     cities << sample_city
-    scalar.times do
+    SCALAR.times do
       towns << sample_city
-      scalar.times do
+      SCALAR.times do
         villages << sample_city
       end
     end
@@ -44,6 +37,14 @@ def generate_addresses(user_id)
     a = Address.new({user_id: user_id, street_address: Faker::Address.street_address, city: city_instance["city"], state: city_instance["state"], zip: Faker::Address.zip_code.to_i})
     a.save
   end
+end
+
+#generate users, 100 users
+(20*SCALAR).times do |x|
+  sample_name = Faker::Name.name
+  u = User.new({id: x, name: sample_name, email: Faker::Internet.email(sample_name), phone_number: Faker::PhoneNumber.phone_number})
+  generate_addresses(u[:id])
+  u.save
 end
 
 #generate orders
