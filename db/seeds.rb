c4a8202eb9ad8595e4bd2761432e49df43c6420b
Andur Carr <carr.andur@gmail.com>
2014-10-03 16:03:15 -0700

    Improve readability and seed ordercontents

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-SCALAR = 5
+SCALAR = 5 # Seed multiplier
 
 #generate products
 sample_categories = []
@@ -16,16 +16,21 @@ SCALAR.times do
 end
 
 (SCALAR**2).times do
-  p = Product.new({title: Faker::Commerce.product_name, category: sample_categories.sample, description: Faker::Lorem.sentence, sku: rand(SCALAR**4)+1, price: Faker::Commerce.price})
+  p = Product.new()
+  p[:title]       = Faker::Commerce.product_name
+  p[:category]    = sample_categories.sample
+  p[:description] = Faker::Lorem.sentence
+  p[:sku]         = rand(SCALAR**4)+1
+  p[:price]       = Faker::Commerce.price
   p.save
 end
 
 #generate addresses, SCALAR+SCALAR^2+SCALAR^3 cities (155)
-def sample_city
+def sample_city # generates a new city/state/zip combination
   {"city" => Faker::Address.city, "state" => Faker::Address.state, "zip" => (Faker::Address.zip_code).to_i}
 end
 
-def sample_cities
+def sample_cities # appends city/state/zip combinations to three echelons in an array, with more instances for each successive echelon
   cities, towns, villages = [], [], []
   SCALAR.times do
     cities << sample_city
@@ -39,51 +44,85 @@ def sample_cities
   [cities, towns, villages]
 end
 
-SAMPLECITIES = sample_cities
+SAMPLECITIES = sample_cities # so that the method is only run once
 
-def random_user_addresses(user_id)
+def random_user_address(user_id)
   address_choices = (Address.select(:id).where(:user_id => user_id)).to_a
-  address_choices[0] ? [address_choices.sample[:id], address_choices.sample[:id]] : [nil, nil]
+  address_choices[0] ? address_choices.sample[:id] : nil
+end
+
+def city_sampling
+  SAMPLECITIES.sample.sample
 end
 
 def generate_addresses(user_id)
   (rand(6)).times do
-    city_instance = SAMPLECITIES.sample.sample
-    a = Address.new({user_id: user_id, street_address: Faker::Address.street_address, city: city_instance["city"], state: city_instance["state"], zip: Faker::Address.zip_code.to_i})
+    city_instance = city_sampling
+    a = Address.new()
+    a[:user_id]        = user_id
+    a[:street_address] = Faker::Address.street_address
+    a[:city]           = city_instance["city"]
+    a[:state]          = city_instance["state"]
+    a[:zip]            = Faker::Address.zip_code.to_i
     a.save
   end
-  random_user_addresses(user_id)
 end
 
 #generate users, 20*SCALAR users (100)
 def creation_date
-
+  time_frames = []
+  SCALAR.times do |x|
+    time_frames << Time.now - ((x+1)*3).month
+  end
+  date_range = (time_frames.sample..Time.now)
+  rand(date_range)
 end
 
 (20*SCALAR).times do |x|
   sample_name = Faker::Name.name
-  address_samples = generate_addresses(x+1)
+  generate_addresses(x+1)
 
-  u = User.new({id: x+1, name: sample_name, email: Faker::Internet.email(sample_name), phone_number: Faker::PhoneNumber.phone_number, default_billing_address: address_samples[0], default_shipping_address: address_samples[1]})
+  u = User.new()
+  u[:id]                       = x+1
+  u[:name]                     = sample_name
+  u[:email]                    = Faker::Internet.email(sample_name)
+  u[:phone_number]             = Faker::PhoneNumber.phone_number
+  u[:default_billing_address]  = random_user_address(x+1)
+  u[:default_shipping_address] = random_user_address(x+1)
+  u[:created_at]               = creation_date
 
   u.save
 end
 
-#generate orders
-def completion(user)
-  user[:default_billing_address] && rand(5) > 0
+#generate order contents
+def generate_contents(order_id)
+  (rand(SCALAR)+1).times do
+    c = OrderContent.new()
+    c[:order_id]   = order_id
+    c[:product_id] = rand(Product.count)+1
+    c[:quantity]   = rand(SCALAR)+1
+    c.save
+  end
 end
 
-def placement_date
-  rand([Time.now - 12.month, Time.now - 8.month, Time.now - 4.month].sample..Time.now)
+#generate orders
+def completion(user)
+  user[:default_billing_address] && rand(5) > 0 # must prevent multiple carts Order.find(user[:id]).where("completed = false")
+end
+
+def placement_date(user)
+  rand(user[:created_at]..Time.now)
 end
 
 (SCALAR*25).times do
   sample_user = User.find(rand(User.count)+1)
-  random_addresses = random_user_addresses(sample_user.id)
   completed_order = completion(sample_user)
-  o = Order.new({user_id: sample_user.id, shipping_address: random_addresses[0], billing_address: random_addresses[1], placed: completed_order, when_placed: placement_date})
+  o = Order.new()
+  o[:user_id]          = sample_user.id
+  o[:shipping_address] = random_user_address(sample_user.id)
+  o[:billing_address]  = random_user_address(sample_user.id)
+  o[:placed]           = completed_order
+  o[:when_placed]      = completed_order ? placement_date(sample_user) : nil
   o.save
+  generate_contents(o[:id])
 end
-
-#generate order contents
