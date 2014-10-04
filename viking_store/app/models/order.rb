0ace978166d9abe299f1334489c4f022f579b697
pndpo <peterdepaulo@gmail.com>
2014-10-04 12:45:41 -0700

    seeds are working

--- viking_store/app/models/order.rb
+++ viking_store/app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ActiveRecord::Base
+	def self.orders_last_week
+		Order.where('is_placed = ? AND placed_at > ?',true,1.week.ago).count
+	end
 end
--- viking_store/db/schema.rb
+++ viking_store/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003125559) do
+ActiveRecord::Schema.define(version: 20141004193836) do
 
   create_table "addresses", force: true do |t|
     t.integer  "user_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20141003125559) do
     t.integer  "quantity"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "current_price"
   end
 
   create_table "orders", force: true do |t|
