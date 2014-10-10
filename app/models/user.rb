cca68e9268b80aebfb82cd63a6ca7b85e0ab9fe3
pndpo <peterdepaulo@gmail.com>
2014-10-09 21:28:43 -0700

    add some model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+	
+	has_many :addresses
+	has_many :orders
+	has_many :order_contents, through: :orders
+	has_many :ordered_products, 
+						through: :order_contents, 
+						source: :products
+
+  before_create do |user|
+    puts "about to create #{user.name}"
+  end
+  after_create :just_created
+
+  protected
+  def just_created
+    puts "just created a user"
+  end
 end
