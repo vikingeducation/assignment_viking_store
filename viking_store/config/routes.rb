06f1001d75b5deddddcca94f0b12bfc0331cd603
Matt Davis <matt.davis129@gmail.com>
2014-10-26 22:26:44 -0700

    creates the admin index page and adds bootstrap to project

--- viking_store/config/routes.rb
+++ viking_store/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'admin#index'
+  
   get '/dashboard' => 'analytics_dashboard#dashboard'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
