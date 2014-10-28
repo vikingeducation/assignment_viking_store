7863b346e6e0a11aa1afb7d1cb5e63a786dc347c
Matt Davis <matt.davis129@gmail.com>
2014-10-28 13:17:57 -0700

    adds create and update methods for new and editing user

--- viking_store/app/controllers/users_controller.rb
+++ viking_store/app/controllers/users_controller.rb
@@ -3,15 +3,29 @@ class UsersController < ApplicationController
 		@users = User.all
 	end
 
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			flash[:success] = "User was created!"
+			redirect_to user_path(@user)
+		else
+			flash.now[:error] = "Woops, I guess he's not coole enough. Didn't save"
+			render :new
+		end
+	end
+
 	def show
 		@user       = User.find(params[:id])
 		@billing    = Address.find(@user.default_billing_id)
 		@shipping   = Address.find(@user.default_shipping_id)
 	end
 
-	def new
-		@user = User.new
-	end
+	
 
 	def show
 		@user = User.find(params[:id])
@@ -19,6 +33,18 @@ class UsersController < ApplicationController
 
 	def edit
 		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+
+		if @user.update(user_params)
+			flash[:success] = "User was updated successfully!"
+			redirect_to user_path(@user)
+		else
+			flash.now[:error] = "That guy didn't save..weird"
+			render :edit
+		end
 	end
 
 
