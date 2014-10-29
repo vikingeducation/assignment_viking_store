60e9718653576db9e28b2ad24ce90f34aad367cd
Matt Davis <matt.davis129@gmail.com>
2014-10-28 19:54:58 -0700

    finishes CRUD on address controller

--- viking_store/app/controllers/addresses_controller.rb
+++ viking_store/app/controllers/addresses_controller.rb
@@ -3,14 +3,39 @@ class AddressesController < ApplicationController
 		@addresses = Address.all
 		@user = User.find(params[:user_id]) if params[:user_id]
 	end
+
+	def new
+  	@address = Address.new
+  	@user = User.find(params[:id])
+  end
+
+  def create
+
+  end
   
   def show
-  	
+  	@address = Address.find(params[:id])
+  	@user = @address.user
   end
 
   def edit
     @address = Address.find(params[:id])
     @user = @address.user
   end
+  
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def address_params
+  	params.require(:address).permit(:street_address, :secondary_address, :city, :state, :zip, :user_id)
+  end
+
 
 end
