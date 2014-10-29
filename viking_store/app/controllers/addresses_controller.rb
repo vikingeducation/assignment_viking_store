0858384aebcbaa76ed7d2cf7ac27895faf355f70
Matt Davis <matt.davis129@gmail.com>
2014-10-28 19:54:40 -0700

    creates edit view and works on controller and index view

--- viking_store/app/controllers/addresses_controller.rb
+++ viking_store/app/controllers/addresses_controller.rb
@@ -1,6 +1,16 @@
 class AddressesController < ApplicationController
 	def index
 		@addresses = Address.all
+		@user = User.find(params[:user_id]) if params[:user_id]
 	end
-	
+  
+  def show
+  	
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+    @user = @address.user
+  end
+
 end
