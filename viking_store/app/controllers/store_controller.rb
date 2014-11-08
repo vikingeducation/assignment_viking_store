3bc223d9a8cf0005ec6275ea169860cf904f8f5e
Matt Davis <matt.davis129@gmail.com>
2014-11-08 12:51:12 -0800

    adds products dynamically to storefront

--- viking_store/app/controllers/store_controller.rb
+++ viking_store/app/controllers/store_controller.rb
@@ -4,6 +4,7 @@ class StoreController < ApplicationController
 
 	def index
     # display text & links
+    @products = Product.all
   end
 
   def login
@@ -21,7 +22,6 @@ class StoreController < ApplicationController
     if found_user
       # mark user as logged in
       session[:user_id] = found_user.id
-      session[:last_name] = found_user.last_name
       flash[:notice] = "You are now logged in."
       redirect_to(:action => 'index')
     else
@@ -33,7 +33,6 @@ class StoreController < ApplicationController
   def logout
     # mark user as logged out
     session[:user_id] = nil
-    session[:last_name] = nil
     flash[:notice] = "Logged out"
     redirect_to(:action => "login")
   end
