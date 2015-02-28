46947d40267d6f646a05e4ff935aea8d037c5692
Dan Berger <dsberger@gmail.com>
2015-02-27 19:59:45 -0800

    user migrate written and done

--- db/migrate/20150228035220_create_users.rb
+++ db/migrate/20150228035220_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :email
+      t.string :name
+      t.integer :customer_profile_id
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228033210) do
+ActiveRecord::Schema.define(version: 20150228035220) do
 
   create_table "product_categories", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20150228033210) do
     t.text     "description"
     t.decimal  "price"
     t.integer  "product_category_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "name"
+    t.integer  "customer_profile_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
   end
