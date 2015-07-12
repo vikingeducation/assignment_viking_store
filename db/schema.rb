73dfb30aaaa38b763f46e4fd27d008fb186066ff
Adam <adam.kinson@gmail.com>
2015-07-12 12:23:39 -0400

    build Order migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712161705) do
+ActiveRecord::Schema.define(version: 20150712161851) do
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "product_details", force: :cascade do |t|
     t.string   "title",               null: false
