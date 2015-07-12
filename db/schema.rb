845434487fe1b8d8642ac82c105d6ce6e9df7419
Adam <adam.kinson@gmail.com>
2015-07-12 12:36:11 -0400

    build Credit Card migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712162611) do
+ActiveRecord::Schema.define(version: 20150712162907) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string   "first_name",                       null: false
+    t.string   "last_name",                        null: false
+    t.string   "street_address_1",                 null: false
+    t.string   "street_address_2"
+    t.string   "city",                             null: false
+    t.string   "state",                            null: false
+    t.string   "zip",                              null: false
+    t.boolean  "default_shipping", default: false, null: false
+    t.boolean  "default_billing",  default: false, null: false
+    t.integer  "user_id",                          null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+  end
 
   create_table "order_products", force: :cascade do |t|
     t.integer  "order_id",   null: false
