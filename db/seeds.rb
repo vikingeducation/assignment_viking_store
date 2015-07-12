415a47b498d85b71d14a249269016735f1698dae
Adam <adam.kinson@gmail.com>
2015-07-12 13:33:20 -0400

    set up seed Users, cities, and states

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,13 +19,39 @@ def clear_seeds
   User.destroy_all
 end
 
+clear_seeds
 
-# Create 100 users
+
+# Create 100 users.
+# This fails but continues looping if email is not unique,
+# which guarantees that I'll end up with the right number
+# of users even if Faker randomly tries to make duplicates
+while User.count < 100 do
+
+  u = User.new
+  u.email = Faker::Internet.email
+  u.first_name = Faker::Name.first_name
+  u.last_name = Faker::Name.last_name
+  u.phone = Faker::PhoneNumber.phone_number
+
+  u.save
+
+end
 
 
 # Create 100 city options (not affected by seed mult)
+city_options = Array.new(100){ Faker::Address.city }.uniq!
+while city_options.size < 100 do
+  city_options << Faker::Address.city
+  city_options.uniq!
+end
 
 # Create 5-10 state options (not affected by seed mult)
+state_options = Array.new(8){ Faker::Address.state_abbr }.uniq!
+while state_options.size < 8 do
+  state_options << Faker::Address.state_abbr
+  state_options.uniq!
+end
 
 
 # Generate 0 to 5 addresses for each user, picking one for default billing & one for default shipping; randomize ZIP codes
