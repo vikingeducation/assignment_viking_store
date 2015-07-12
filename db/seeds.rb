1fc20f00c33a52c8ccaef43cebdb5d2222691155
Adam <adam.kinson@gmail.com>
2015-07-12 14:53:50 -0400

    set up seeds for addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,14 +40,14 @@ end
 
 
 # Create 100 city options (not affected by seed mult)
-city_options = Array.new(100){ Faker::Address.city }.uniq!
+city_options = Array.new(100){ Faker::Address.city }.uniq
 while city_options.size < 100 do
   city_options << Faker::Address.city
   city_options.uniq!
 end
 
 # Create 5-10 state options (not affected by seed mult)
-state_options = Array.new(8){ Faker::Address.state_abbr }.uniq!
+state_options = Array.new(8){ Faker::Address.state_abbr }.uniq
 while state_options.size < 8 do
   state_options << Faker::Address.state_abbr
   state_options.uniq!
@@ -55,7 +55,31 @@ end
 
 
 # Generate 0 to 5 addresses for each user, picking one for default billing & one for default shipping; randomize ZIP codes
+User.all.each do |user|
 
+  rand(0..5).times do |iteration|
+    a = Address.new
+    a.first_name = user.first_name
+    a.last_name = user.last_name
+    a.street_address_1 = Faker::Address.street_address
+    a.street_address_2 = Faker::Address.secondary_address if rand < 0.4
+    a.city = city_options.sample
+    a.state = state_options.sample
+    a.zip = Faker::Address.zip[0..4]
+    a.default_shipping = false
+    a.default_billing = false
+    a.user_id = user.id
+
+    a.save!
+  end
+
+  user_addresses = Address.where(:user_id => user.id)
+  unless user_addresses.size.empty?
+    user_addresses.sample.update(:default_shipping => true)
+    user_addresses.sample.update(:default_billing => true)
+  end
+
+end
 
 
 
