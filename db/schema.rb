65c14bd3d673fa9da0881e0d0d42c77028012d10
Adam <adam.kinson@gmail.com>
2015-07-12 12:18:17 -0400

    build Product Detail migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712160833) do
+ActiveRecord::Schema.define(version: 20150712161129) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "sku",               null: false
+    t.integer  "product_detail_id", null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  add_index "products", ["sku"], name: "index_products_on_sku"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",      null: false
