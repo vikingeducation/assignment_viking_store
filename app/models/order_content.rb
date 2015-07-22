63eda2c099c1f129435177e5c1e38c1fc6e9f48d
Dan Berger <dsberger@gmail.com>
2015-07-22 09:09:53 -0700

    changes within OrderContent model

--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -1,4 +1,4 @@
-class OrderContents < ActiveRecord::Base
+class OrderContent < ActiveRecord::Base
 
 
   belongs_to :order
@@ -16,12 +16,12 @@ class OrderContents < ActiveRecord::Base
   def self.revenue(last_x_days = nil)
 
     if last_x_days
-      joins("JOIN products ON order_contents.product_id = products.id JOIN orders ON orders.id = order_contents.order_id").
+      joins("JOIN products ON order_content.product_id = products.id JOIN orders ON orders.id = order_content.order_id").
       where("orders.checkout_date > ?", Time.now - last_x_days.days).
-      sum("(order_contents.quantity * products.price)")
+      sum("(order_content.quantity * products.price)")
     else
-      joins("JOIN products ON order_contents.product_id = products.id JOIN orders ON orders.id = order_contents.order_id").
-      sum("(order_contents.quantity * products.price)")
+      joins("JOIN products ON order_content.product_id = products.id JOIN orders ON orders.id = order_content.order_id").
+      sum("(order_content.quantity * products.price)")
     end
 
   end
