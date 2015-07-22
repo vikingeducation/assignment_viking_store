25f914ce6bb616467e792f8bfed2b284551df8ed
Dan Berger <dsberger@gmail.com>
2015-07-22 09:34:08 -0700

    more edits to OrderContent model

--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -16,12 +16,12 @@ class OrderContent < ActiveRecord::Base
   def self.revenue(last_x_days = nil)
 
     if last_x_days
-      joins("JOIN products ON order_content.product_id = products.id JOIN orders ON orders.id = order_content.order_id").
+      joins("JOIN products ON order_contents.product_id = products.id JOIN orders ON orders.id = order_contents.order_id").
       where("orders.checkout_date > ?", Time.now - last_x_days.days).
-      sum("(order_content.quantity * products.price)")
+      sum("(order_contents.quantity * products.price)")
     else
-      joins("JOIN products ON order_content.product_id = products.id JOIN orders ON orders.id = order_content.order_id").
-      sum("(order_content.quantity * products.price)")
+      joins("JOIN products ON order_contents.product_id = products.id JOIN orders ON orders.id = order_contents.order_id").
+      sum("(order_contents.quantity * products.price)")
     end
 
   end
