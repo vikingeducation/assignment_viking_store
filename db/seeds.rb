29ea8ed2c59fe351a2d0fb2a57c4a2b61037e090
shadefinale <shadefinale@gmail.com>
2015-07-23 18:41:54 -0500

    Finished seeding data generation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,8 +22,8 @@ end
 		        email: Faker::Internet.email,
 		        number: Faker::PhoneNumber.phone_number,
 		        cc_num: Faker::Business.credit_card_number,
-			    	default_billing_address_id: Location.where(:user_id => i+1).sample.id,
-			    	default_shipping_address_id: Location.where(:user_id => i+1).sample.id)
+		    	default_billing_address_id: Location.where(:user_id => i+1).pluck(:id).sample,
+		    	default_shipping_address_id: Location.where(:user_id => i+1).pluck(:id).sample)
 end
 
 
@@ -37,13 +37,32 @@ end
 		             description: Faker::Lorem.sentence,
 		             price: rand()*20+10,
 		             sku: Faker::Code.ean,
-		             category_id: rand*10+1)
+		             category_id: Category.pluck(:id).sample)
 end
 
 70.times do
-	Order.new
-	Order.user = rand*50+1
-	Order.shipping_address_id = Location.where(:user_id => Order.user_id).sample.id
+	new_order = Order.new
+	rand_user_id = rand*50+1
+	new_order.user_id = rand_user_id
+	new_order.shipping_address_id = Location.where(
+		                            :user_id => rand_user_id).pluck(:id).sample
+	new_order.billing_address_id  = Location.where(
+		                            :user_id => rand_user_id).pluck(:id).sample
+	# We will pretend 90% of orders have been bought and the other 10% are carts.
+	unless rand * 10 + 1 == 10
+	new_order.checkout_date = Time.parse('2015-07-23 00:00:00') - (rand * 86400 * 365 + 1).to_i
+	end
+	new_order.save
+end
+
+140.times do
+	rand_order_id = rand*70 + 1
+	rand_product_id = rand*100+1
+	rand_qty = rand*10
+	OrderProduct.create(order_id: rand_order_id,
+                        product_id: rand_product_id,
+                        quantity: rand_qty)
+end
 
 
 
