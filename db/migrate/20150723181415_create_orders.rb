cd10e980f82fbbb77622a0232f1cf53165bc339a
Alice Fung <alice.o.fung@gmail.com>
2015-07-23 18:52:45 -0400

    add faker methods for some models

--- db/migrate/20150723181415_create_orders.rb
+++ db/migrate/20150723181415_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.integer :user_id, null: false
-      t.datetime :check_out, null: false
+      t.datetime :check_out
       t.timestamps null: false
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-MULTIPLIER = 10
+MULTIPLIER = 1
+
 city_name = Array.new(100){Faker::Address.city}
 state_name = Array.new(10){Faker::Address.state}
 
 def create_users(start_date, end_date)
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name  , email: Faker::Internet.email, created_at: Faker::Date.between(start_date, end_date))
+end
+
+def create_address
+  Address.create(street: Faker::Address.street_address, city: city_name.sample, state: state_name.sample, zip_code: Faker::Address.zip_code)
+end
+
+def credit_card_num
+   Faker::Business.credit_card_number.split("-").join("").to_i
+end
+
+
+def create_billing(user_num, address_num)
+  Billing.create(user_id: user_num, address_id: address_num, cc_num: credit_card_num, cc_type: Faker::Business.credit_card_type, cc_exp: Faker::Business.credit_card_expiry_date)
 end
 
 Rake::Task['db:reset'].invoke
@@ -23,10 +37,34 @@ Rake::Task['db:reset'].invoke
   3.times {create_users(1.years.ago, Time.now)}
 end
 
+#0-5 addresses for users
+# default for shipping if selected
+# user-address table first??
+# generate address first??? address table
 
 
-#10-30 products
+# method to randomly assign users a # of addresses???
+
+
+
+
+
+
+
+
 #~6 categories
+cat_size = 6
+cat_size.times {Categories.create(Faker::Commerce.department)}
+
+#10-30 products/category
+#make product
+def product_for_category(cat_id)
+  rand(10..30).times do |i|
+    Product.new(name: Faker::Commerce.product_name, category_id: cat_id, price: Faker::Commerce.price, description: "some description", stock: rand(0..100))
+end
+
+#populates category with products
+cat_size.downto(1) { |num| product_for_category(num) }
 
 #100 orders in past year (growth rate)
 
