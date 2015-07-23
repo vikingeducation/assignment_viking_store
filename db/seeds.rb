19891d94abc84efa34396afa0d8c670c17fe6d80
facingsouth <xin.d.liu@gmail.com>
2015-07-23 15:54:42 -0700

    seed product category

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 srand(115101101100)
-250.times do
-	Location.create(user_id: rand() * 50 + 1,
+250.times do |i|
+	Location.create(user_id: i/5+1,
 		            address: Faker::Address.street_address,
 		            city: Faker::Address.city,
 		            state: Faker::Address.state,
@@ -22,6 +22,44 @@ end
 		        email: Faker::Internet.email,
 		        number: Faker::PhoneNumber.phone_number,
 		        cc_num: Faker::Business.credit_card_number,
-		    	default_billing_address_id: rand * 250 + 1,
-		    	default_shipping_address_id: rand * 250 + 1)
+			    	default_billing_address_id: Location.where(:user_id => i+1).sample.id,
+			    	default_shipping_address_id: Location.where(:user_id => i+1).sample.id)
 end
+
+
+10.times do
+	Category.create(description: Faker::Hacker.noun)
+end
+
+
+100.times do
+	Product.create(title: Faker::Commerce.product_name,
+		             description: Faker::Lorem.sentence,
+		             price: rand()*20+10,
+		             sku: Faker::Code.ean,
+		             category_id: rand*10+1)
+end
+
+70.times do
+	Order.new
+	Order.user = rand*50+1
+	Order.shipping_address_id = Location.where(:user_id => Order.user_id).sample.id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
