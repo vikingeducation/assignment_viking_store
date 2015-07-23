a4a0b1fd646bd48de3f079cfc6a8ae189051546c
facingsouth <xin.d.liu@gmail.com>
2015-07-23 15:03:50 -0700

    add order products table

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ActiveRecord::Base
   belongs_to :shipping_address, :class_name => "Location"
   belongs_to :billing_address, :class_name => "Location"
   belongs_to :user
+  has_and_belongs_to_many :products, :join_table => "order_products"
+  has_many :order_products
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723183244) do
+ActiveRecord::Schema.define(version: 20150723213536) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "description"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20150723183244) do
     t.string   "state"
     t.string   "country"
     t.integer  "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_products", force: :cascade do |t|
+    t.integer  "order_id"
+    t.integer  "product_id"
+    t.integer  "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
