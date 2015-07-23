bbe3e8b803022be784dc87fb7ffd9a82e8350165
mikebutts <mgbutts@gmail.com>
2015-07-23 19:07:54 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -70,12 +70,4 @@ ActiveRecord::Schema.define(version: 20150723213535) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name", null: false
-    t.string   "last_name",  null: false
-    t.string   "email",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
