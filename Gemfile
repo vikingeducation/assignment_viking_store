b1f43104e01f8c31570ea7ab395ae49741281ee9
shadefinale <shadefinale@gmail.com>
2015-07-23 17:29:38 -0500

    Working on seeding database with faker.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+
 gem 'faker'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.2'
@@ -35,7 +36,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-
+  gem 'jazz_hands', github: 'nixme/jazz_hands', branch: 'bring-your-own-debugger'
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+srand(115101101100)
+250.times do
+	Location.create(user_id: rand() * 50 + 1,
+		            address: Faker::Address.street_address,
+		            city: Faker::Address.city,
+		            state: Faker::Address.state,
+		            country: Faker::Address.country,
+		            zipcode: Faker::Address.zip.to_i)
+end
+
+
+
+50.times do |i|
+	User.create(name: Faker::Name.name,
+		        email: Faker::Internet.email,
+		        number: Faker::PhoneNumber.phone_number,
+		        cc_num: Faker::Business.credit_card_number,
+		    	default_billing_address_id: rand * 250 + 1,
+		    	default_shipping_address_id: rand * 250 + 1)
+end
