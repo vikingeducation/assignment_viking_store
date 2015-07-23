cd6d6553b852ba38926612a3a9c7933d1eca6813
ayva <olga.ayva@gmail.com>
2015-07-23 16:05:38 -0700

    All tables filled

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,14 +21,14 @@ end
   User.create(first_name:  Faker::Name.first_name,
               last_name: Faker::Name.last_name,
               email: Faker::Internet.email,
-              password: "#{rand(0,9)}#{last_name}")
+              password: "#{rand(0..9)}#{Faker::Name.last_name}")
             end
 
 
 #Generate Addresses
 
 30.times do
-    Address.create(ap: rand(1,999),
+    Address.create(ap: rand(1..999),
                   bld: Faker::Address.building_number,
                   street: Faker::Address.street_name,
                   city: Faker::Address.city ,
@@ -39,13 +39,12 @@ end
 
 #Generate Billing
 15.times do 
-    Billings.create(creditcard: Faker::Business.credit_card_number,
+    Billing.create( creditcard: Faker::Business.credit_card_number,
                     first_name:  Faker::Name.first_name,
                     last_name: Faker::Name.last_name,
                     exp: Faker::Business.credit_card_expiry_date,
-                    cvc: "#{rand(0,9)}#{rand(0,9)}#{rand(0,9)}",
-                    type: Faker::Business.credit_card_type,
-
+                    "CVC" => "#{rand(0..999)}",
+                    type: Faker::Business.credit_card_type
               )
   end
 
@@ -70,23 +69,51 @@ sku_array = get_unique_sku
   Product.create( title: Faker::Commerce.product_name,
                   price: Faker::Commerce.price,
                   sku: sku_array[i],
-                  category_id: rand(20),
+                  category_id: rand(1..20),
                   amount_in_stock: rand(0..999),
     )
-
+end
 
 # Generate orders
+10.times do 
+  Order.create(billing_id: rand(1..15),
+                buyer_id: rand(1..20),
+                shipping_address_id: rand(1..30)             
+    )
+
+end
+
+# Generate products_orders and Generate User_billings
+10.times do
+  num=rand(1..10)
+  rand(1..5).times do
+    Product_order.create( order_id: num,
+                          product_id: rand(1..50),
+                          product_quantity: rand(1..3),
+      )
+  end
+  User_billing.create(buyer_id: Order.find(num).buyer_id,
+                      billing_id: rand(1..15)
+                      )
+end
 
 
 # Generate Shipments
-# 20.times do
-#   user = User.find(rand(0...30))
-#   user.default_shipping_address = rand(0...30)
-#   Shipment.create(user_id: user.id,
-#                   shipment_address_id: user.default_shipping_address,
-#                   pack_date: user.created_at,
-#     )
-#
+20.times do
+  order = Order.find(rand(1...30))
+  user.default_shipping_address = rand(1...30)
+  Shipment.create(user_id: order.buyer_id,
+                  shipment_address_id: user.default_shipping_address,
+                  pack_date: order.created_at
+                  )
+end
+
+
+
+
+
+
+
 
 
 
