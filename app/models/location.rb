05d08266e69a2259e65f85419525e486fe13d9ba
shadefinale <shadefinale@gmail.com>
2015-07-23 13:14:43 -0500

    Belongs to singular rather than plural.

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,5 @@ class Location < ActiveRecord::Base
   has_many :billing_addresses, class_name: "User", foreign_key: "default_billing_address_id"
   has_many :shipping_addresses, class_name: "Order", foreign_key: "shipping_address_id"
   has_many :billing_addresses, class_name: "Order", foreign_key: "billing_address_id"
+  belongs_to :user
 end
