389b6fb4cd0640a6938aa8a02d32a38226201d95
Alice Fung <alice.o.fung@gmail.com>
2015-07-23 21:18:04 -0400

    spacing

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,17 @@ CITY_NAME = Array.new(100){Faker::Address.city}
 STATE_NAME = Array.new(10){Faker::Address.state}
 
 def create_users(start_date, end_date)
-  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name  , email: Faker::Internet.email, created_at: Faker::Date.between(start_date, end_date))
+  User.create(first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name  ,
+              email: Faker::Internet.email,
+              created_at: Faker::Date.between(start_date, end_date))
 end
 
 def create_address
-  Address.create(street: Faker::Address.street_address, city: CITY_NAME.sample, state: STATE_NAME.sample, zip_code: Faker::Address.zip_code)
+  Address.create(street: Faker::Address.street_address,
+                city: CITY_NAME.sample,
+                state: STATE_NAME.sample,
+                zip_code: Faker::Address.zip_code)
 end
 
 # def credit_card_num
@@ -26,7 +32,11 @@ end
 
 
 def create_billing(user_num, address_num)
-  Billing.create(user_id: user_num, address_id: address_num, cc_number: Faker::Business.credit_card_number, cc_type: Faker::Business.credit_card_type, cc_exp: Faker::Business.credit_card_expiry_date)
+  Billing.create(user_id: user_num,
+            address_id: address_num,
+            cc_number: Faker::Business.credit_card_number,
+            cc_type: Faker::Business.credit_card_type,
+            cc_exp: Faker::Business.credit_card_expiry_date)
 end
 
 
@@ -48,11 +58,14 @@ end
 #0-5 addresses for users
 users = User.all
 addresses = Address.all
+
 users.each do |user|
   user_num_addy = rand(0..5)
   user_addy = addresses.sample(user_num_addy)
+
   user_num_addy.times do |i|
-    UserAddress.create(user_id: user.id, address_id: user_addy[i].id)
+    UserAddress.create(user_id: user.id,
+                        address_id: user_addy[i].id)
     user.default_shipping ||= user_addy[i].id
   end
 end
@@ -73,7 +86,11 @@ cat_size.times {Category.create(name: Faker::Commerce.department)}
 #make product
 def product_for_category(cat_id)
   rand(10..30).times do |i|
-    Product.create(name: Faker::Commerce.product_name, category_id: cat_id, price: Faker::Commerce.price, description: "some description", stock: rand(0..100))
+    Product.create( name: Faker::Commerce.product_name,
+                    category_id: cat_id,
+                    price: Faker::Commerce.price,
+                    description: "some description",
+                    stock: rand(0..100))
   end
 end
 
@@ -84,8 +101,11 @@ products = Product.all
 #create order first, then packinglist
 
 def user_make_order(userid)
-  user_order = Order.create(user_id: userid, checkout: Faker::Date.between(1.years.ago, Time.now))
-  PackingList.create(product_id: products.sample.id, order_id: user_order.id, quantity: rand(1..10))
+  user_order = Order.create(user_id: userid,
+                checkout: Faker::Date.between(1.years.ago, Time.now))
+
+  PackingList.create(product_id: products.sample.id,
+                    order_id: user_order.id, quantity: rand(1..10))
 end
 
 #100 orders in past year (growth rate)
