8c774ed81ef58f2fb11da87df874e0ffa6fcdce8
Sia <siakaramalegos@gmail.com>
2015-12-25 12:42:31 -0600

    Adds line item model and relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ActiveRecord::Base
   belongs_to :shipping_address
   belongs_to :billing_address
   belongs_to :credit_card
+  has_many :line_items
 end
---
+++ db/migrate/20151225184039_create_line_items.rb
@@ -0,0 +1,13 @@
+class CreateLineItems < ActiveRecord::Migration
+  def change
+    create_table :line_items do |t|
+      t.references :product, index: true, foreign_key: true, null: false
+      t.integer :quantity, null: false, default: 1
+      t.decimal :price, null: false
+      t.references :cart, index: true, foreign_key: true
+      t.references :order, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
