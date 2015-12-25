c23b64603dfed4d29af6bad05deea328df31b902
Sia <siakaramalegos@gmail.com>
2015-12-25 12:25:15 -0600

    Adds city and state models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151225181513) do
+ActiveRecord::Schema.define(version: 20151225181943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20151225181513) do
     t.string   "description", null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "credit_cards", force: :cascade do |t|
@@ -45,6 +51,13 @@ ActiveRecord::Schema.define(version: 20151225181513) do
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "states", force: :cascade do |t|
+    t.string   "name",         null: false
+    t.string   "abbreviation", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",        null: false
