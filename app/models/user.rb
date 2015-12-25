31beb3131e37bd6735a925628f66c20f72e337a9
Sia <siakaramalegos@gmail.com>
2015-12-25 17:12:44 -0600

    Adds todo to change user to only have one cart once code is ready

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :credit_cards
   has_many :addresses
+  # TODO: change to has_one cart once cart-to-order process properly set up
   has_many :carts
   has_many :orders
 end
