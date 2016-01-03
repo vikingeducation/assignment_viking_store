3774db62f70e3fa5a744190639a27dee0743c410
Sia <siakaramalegos@gmail.com>
2016-01-03 12:18:17 -0600

    Adds associations, queries, and scopes from project version of app

--- app/models/cart.rb
+++
@@ -1,4 +0,0 @@
-class Cart < ActiveRecord::Base
-  belongs_to :user
-  has_many :line_items
-end
--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -1,5 +1,4 @@
 class OrderContent < ActiveRecord::Base
   belongs_to :product
-  belongs_to :cart
   belongs_to :order
 end
