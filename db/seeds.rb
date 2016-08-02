15ca8cac92a47376ea4a528b46bdda3699a439db
Leo Ahnn <leoahnn@gmail.com>
2016-08-02 18:43:02 -0400

    finished seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,14 @@
 
 # delete
 
-#User.destroy_all
-#Product.destroy_all
-#CreditCard.destroy_all
-#Address.destroy_all
-#Order.destroy_all
-#Category.destroy_all
-#PhoneNumber.destroy_all
-#Shipment.destroy_all
+User.destroy_all
+Product.destroy_all
+CreditCard.destroy_all
+Address.destroy_all
+Order.destroy_all
+Category.destroy_all
+PhoneNumber.destroy_all
+Shipment.destroy_all
 
 #User/address
 #User/phonenumber
@@ -63,19 +63,19 @@ address_array = Address.select('id').to_a
   Shipment.create(address_id: id)
 end
 
-10.times do 
+10.times do
   id = user_array.sample.id
   Order.create(user_id: id)
 end
 
 phone_numbers = PhoneNumber.select("id").to_a
-10.times do 
+10.times do
   user_id = user_array.sample.id
   address_id = address_array.sample.id
   UserAddress.create(user_id: user_id, address_id: address_id)
 end
 
-10.times do 
+10.times do
   phone_number_id = phone_numbers.sample.id
   user_id = user_array.sample.id
   UserPhoneNumber.create(user_id: user_id, phone_number_id: phone_number_id )
@@ -83,7 +83,7 @@ end
 
 products_array = Product.select('id').to_a
 orders_array = Order.select('id').to_a
-10.times do 
+10.times do
   product_id = products_array.sample
   order_id = orders_array.sample
   ProductOrder.new(product_id: product_id, order_id: order_id )
