75f0005aef1919aa2d7b62739742f60bc9746fab
Dylan Lynch <lynchd2@tcnj.edu>
2016-08-02 18:26:50 -0400

    Finish seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802204725) do
+ActiveRecord::Schema.define(version: 20160802222018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 20160802204725) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "kind"
   end
 
   create_table "credit_cards", force: :cascade do |t|
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160802204725) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "phone_numbers", force: :cascade do |t|
@@ -47,7 +48,7 @@ ActiveRecord::Schema.define(version: 20160802204725) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "product_order", force: :cascade do |t|
+  create_table "product_orders", force: :cascade do |t|
     t.integer "product_id"
     t.integer "order_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,19 +13,22 @@
 
 # delete
 
-User.destroy_all
-Product.destroy_all
-CreditCard.destroy_all
-Address.destroy_all
-Order.destroy_all
-Category.destroy_all
-PhoneNumber.destroy_all
-Shipment.destroy_all
+#User.destroy_all
+#Product.destroy_all
+#CreditCard.destroy_all
+#Address.destroy_all
+#Order.destroy_all
+#Category.destroy_all
+#PhoneNumber.destroy_all
+#Shipment.destroy_all
 
+#User/address
+#User/phonenumber
+#Product/Order
 
 
 10.times do
-  User.create(name: Fake::Name.name, email: Faker::Internet.email)
+  User.create(name: Faker::Name.name, email: Faker::Internet.email)
 end
 
 10.times do
@@ -47,7 +50,7 @@ user_array = User.select('id').to_a
 end
 
 10.times do
-  Catagory.create(type: Faker::Color.color_name)
+  Category.create(kind: Faker::Color.color_name)
 end
 
 10.times do
@@ -57,5 +60,31 @@ end
 address_array = Address.select('id').to_a
 10.times do
   id = address_array.sample.id
-  Shipments.create(address_id: id)
+  Shipment.create(address_id: id)
+end
+
+10.times do 
+  id = user_array.sample.id
+  Order.create(user_id: id)
+end
+
+phone_numbers = PhoneNumber.select("id").to_a
+10.times do 
+  user_id = user_array.sample.id
+  address_id = address_array.sample.id
+  UserAddress.create(user_id: user_id, address_id: address_id)
+end
+
+10.times do 
+  phone_number_id = phone_numbers.sample.id
+  user_id = user_array.sample.id
+  UserPhoneNumber.create(user_id: user_id, phone_number_id: phone_number_id )
+end
+
+products_array = Product.select('id').to_a
+orders_array = Order.select('id').to_a
+10.times do 
+  product_id = products_array.sample
+  order_id = orders_array.sample
+  ProductOrder.new(product_id: product_id, order_id: order_id )
 end
