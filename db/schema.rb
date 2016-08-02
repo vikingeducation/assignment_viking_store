0031d459889404dc99489e3628794c5f8cbcb406
Morgan Martin <morganm5201@gmail.com>
2016-08-02 18:25:28 -0400

    begin seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802213903) do
+ActiveRecord::Schema.define(version: 20160802214815) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id"
@@ -22,11 +22,35 @@ ActiveRecord::Schema.define(version: 20160802213903) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "credit_cards", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "cc_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.boolean  "cart"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "price"
+    t.integer  "sku_num"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+f_address  = Faker::Address
+f_internet = Faker::Internet
+f_name     = Faker::Name
+f_phone_number = Faker::PhoneNumber
+f_business = Faker::Business
+
+## Create some users
+
+100.times do
+  User.create({
+                email: f_internet.email,
+                first_name: f_name.first_name,
+                last_name: f_name.last_name,
+                phone_num: f_phone_number.phone_number
+                })
+end
+
+## Create some creditcards
+
+75.times do
+  users = User.order("RANDOM()")
+  rand_user_id = users.pop.id
+  CreditCard.create({
+                      user_id: rand_user_id,
+                      cc_number: f_business.credit_card_number
+                    })
+end
+
+
+## Create some addresses
+
+def get_rand_user_for_addr
+  begin
+    rand_user = User.order("RANDOM()").first
+  end while rand_user.ship_addr_id && rand_user.bill_addr_id
+  rand_user
+end
+
+75.times do
+  Address.create({
+                   user_id: get_rand_user_for_addr.id,
+                   
+
+
+                   })
+
+end
