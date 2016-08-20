4395df5d4445eecd32f5a2893b635b3169089fbb
BranLiang <lby89757@hotmail.com>
2016-08-20 19:20:06 +0800

    add states table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820110823) do
+ActiveRecord::Schema.define(version: 20160820111839) do
+
+  create_table "states", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_states_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",  null: false
