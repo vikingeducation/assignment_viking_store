10b8d2c65dbaf8e8d0eb061c4223c105a2ae597e
BranLiang <lby89757@hotmail.com>
2016-08-20 15:57:10 +0800

    add address

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,10 +22,46 @@ Address.destroy_all
 10.times do
   name = Faker::Name.name
   email = Faker::Internet.email("#{name}")
-  user = User.create(:email => email, :name => name )
+  user = User.create(
+  :email => email,
+  :name => name
+  )
 
+  # add PhoneNumber
   phone_number = Faker::PhoneNumber.cell_phone
-  user.user_details.create(:phone_number => phone_number)
+  user.user_details.create(
+  :phone_number => phone_number
+  )
 
-  
+  # add credit_cards
+  3.times do
+    credit_card_number = Faker::Business.credit_card_number
+    expire_date = Faker::Business.credit_card_expiry_date
+    user.user_details.first.credit_cards.create(
+    :card_number => credit_card_number,
+    :mvc_number => rand(100..999).to_s,
+    :expire_date => expire_date
+    )
+  end
+
+  # add addresses
+  5.times do
+    country = Faker::Address.country
+    province = Faker::Address.state
+    city = Faker::Address.city
+    postcode = Faker::Address.postcode
+    address_1 = Faker::Address.street_address
+    address_2 = Faker::Address.secondary_address
+    user.user_details.first.addresses.create(
+    :country => country,
+    :province => province,
+    :city => city,
+    :postcode => postcode,
+    :address_1 => address_1,
+    :address_2 => address_2,
+    )
+    user.user_details.first.addresses.first.billing_default = true
+    user.user_details.first.addresses.first.shipping_default = true
+  end
+
 end
