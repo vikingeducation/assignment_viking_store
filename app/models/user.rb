9f8d6e29de2341a1d6d4f8af429ad0d6656ba3c1
BranLiang <lby89757@hotmail.com>
2016-08-20 19:37:05 +0800

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  # User has many addresses
+  # But only one default billing address
+  # As the same for the shipping address
+  has_many :addresses
+
+  belongs_to :billing_address,
+             class_name: "Address",
+             :foreign_key => :billing_id
+
+  belongs_to :shipping_address,
+             class_name: "Address",
+             :foreign_key => :shipping_id
 end
