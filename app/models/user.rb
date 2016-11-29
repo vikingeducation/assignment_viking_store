38e32d752386e9579856e44eeab1eb9dc89e5fb2
Pramod Jacob <psjacob68@gmail.com>
2016-11-29 18:27:15 -0500

    working on seeds.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  has_many :addresses
+  has_many :orders
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# user = User.create([{
+#   email: Faker::Internet.email,
+#   first_name: Faker::Name.first_name,
+#   last_name: Faker::Name.last_name,
+#   phone_number: Faker::PhoneNumber.phone_number
+# }])
+#
+# user.create_address([{
+#   street: Faker::Address.street_address,
+#   zip: Faker::Address.zip,
+#   type: 'shipping',
+#   primary: false,
+#   user_id: user.id
+# }])
