a32500872df62640ab9ef23843857ed193dfd480
Pramod Jacob <psjacob68@gmail.com>
2016-11-29 17:29:08 -0500

    added migrations

--- db/migrate/20161129195959_add_address_relationships.rb
+++ db/migrate/20161129195959_add_address_relationships.rb
@@ -1,5 +1,8 @@
 class AddAddressRelationships < ActiveRecord::Migration[5.0]
   def change
-    add_reference 
+    add_reference :addresses, :users, :foreign_key => true
+    add_reference :cities, :states, :foreign_key => true
+    add_reference :states, :countries, :foreign_key => true
+    add_reference :addresses, :cities, :foreign_key => true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129195720) do
+ActiveRecord::Schema.define(version: 20161129222626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,69 @@ ActiveRecord::Schema.define(version: 20161129195720) do
     t.boolean  "primary",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "users_id"
+    t.integer  "cities_id"
+    t.index ["cities_id"], name: "index_addresses_on_cities_id", using: :btree
+    t.index ["users_id"], name: "index_addresses_on_users_id", using: :btree
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "cities", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "states_id"
+    t.index ["states_id"], name: "index_cities_on_states_id", using: :btree
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "quantity"
+    t.float    "sale_price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "products_id"
+    t.integer  "orders_id"
+    t.index ["orders_id"], name: "index_line_items_on_orders_id", using: :btree
+    t.index ["products_id"], name: "index_line_items_on_products_id", using: :btree
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "date"
+    t.boolean  "open"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "users_id"
+    t.index ["users_id"], name: "index_orders_on_users_id", using: :btree
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.float    "price"
+    t.string   "sku"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "categories_id"
+    t.index ["categories_id"], name: "index_products_on_categories_id", using: :btree
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string   "name",         null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "countries_id"
+    t.index ["countries_id"], name: "index_states_on_countries_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +97,12 @@ ActiveRecord::Schema.define(version: 20161129195720) do
     t.index ["id"], name: "index_users_on_id", unique: true, using: :btree
   end
 
+  add_foreign_key "addresses", "cities", column: "cities_id"
+  add_foreign_key "addresses", "users", column: "users_id"
+  add_foreign_key "cities", "states", column: "states_id"
+  add_foreign_key "line_items", "orders", column: "orders_id"
+  add_foreign_key "line_items", "products", column: "products_id"
+  add_foreign_key "orders", "users", column: "users_id"
+  add_foreign_key "products", "categories", column: "categories_id"
+  add_foreign_key "states", "countries", column: "countries_id"
 end
