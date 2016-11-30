df119b93abd26bc4c3619410c379a8a29ffb4a2f
Jessica G <gillan.jessica@gmail.com>
2016-11-29 17:34:38 -0700

    finish and run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,79 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129231233) do
+ActiveRecord::Schema.define(version: 20161129235154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string   "street_address"
+    t.string   "city"
+    t.string   "state"
+    t.integer  "zip"
+    t.string   "country"
+    t.boolean  "default"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts_products", id: false, force: :cascade do |t|
+    t.integer "cart_id",    null: false
+    t.integer "product_id", null: false
+    t.integer "quantity"
+    t.index ["cart_id", "product_id"], name: "index_carts_products_on_cart_id_and_product_id", using: :btree
+    t.index ["product_id", "cart_id"], name: "index_carts_products_on_product_id_and_cart_id", using: :btree
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "credit_cards", force: :cascade do |t|
+    t.string   "number"
+    t.string   "exp"
+    t.string   "csv"
+    t.integer  "user_id"
+    t.integer  "billing_address_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "shipping_addr_id"
+    t.integer  "billing_addr_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "orders_products", id: false, force: :cascade do |t|
+    t.integer "order_id",   null: false
+    t.integer "product_id", null: false
+    t.integer "quantity"
+    t.float   "cost"
+    t.index ["order_id", "product_id"], name: "index_orders_products_on_order_id_and_product_id", using: :btree
+    t.index ["product_id", "order_id"], name: "index_orders_products_on_product_id_and_order_id", using: :btree
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.float    "price"
+    t.integer  "sku"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Create 100 users staggered over time, with an increasing join rate
+# as time goes on
+
+User.create({email: "Faker", name: "faker"})
