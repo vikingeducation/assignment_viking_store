cd4c8666e9b6cd2f975e116c26a10aeac5e5bf0e
efraimmgon <efraimmaugusto@gmail.com>
2017-04-18 08:31:49 -0400

    make sure seeds.rb resets the data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def reset_database
+  User.delete_all
+  Profile.delete_all
+  CreditCard.delete_all
+  Address.delete_all
+  Product.delete_all
+  Category.delete_all
+  Order.delete_all
+  OrderProduct.delete_all
+end
+
+# Run
+puts "Starting seeds...\n\n"
+
+puts "Removing old data..."
+reset_database
+puts "Old data removed.\n\n"
