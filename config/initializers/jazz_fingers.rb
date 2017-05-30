fedc4c517a8e87a06d977449d014dc3797d28ff2
Ann Allan <ann.cristina.allan@gmail.com>
2017-05-30 18:19:27 -0400

    Add models

--- config/initializers/jazz_fingers.rb
+++ config/initializers/jazz_fingers.rb
@@ -3,7 +3,7 @@ if defined?(JazzFingers)
     config.colored_prompt = true
     config.awesome_print = true
     config.coolline = false
-    config.application_name = Rails5Sandbox
+    config.application_name = VikingStore
   end
 
   require 'jazz_fingers/setup'
---
+++ test/models/bill_add_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BillAddTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
