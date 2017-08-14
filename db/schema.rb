1d6e289dc7c8f5bc9c367d34057efd3db2d8a8c0
Roy Chen <roychenlei@gmail.com>
2017-08-14 14:34:46 +0800

    added CreditCard model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814060428) do
+ActiveRecord::Schema.define(version: 20170814063309) do
 
   create_table "address_types", force: :cascade do |t|
     t.string "address_type"
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 20170814060428) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "credit_cards", force: :cascade do |t|
+    t.string "card_number"
+    t.date "expiry_date"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_credit_cards_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
