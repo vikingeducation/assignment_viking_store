128e5b6b9f2dff130acfefe1953975e673008d73
Roy Chen <roychenlei@gmail.com>
2017-08-14 13:55:35 +0800

    added Order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814020801) do
+ActiveRecord::Schema.define(version: 20170814035424) do
 
   create_table "address_types", force: :cascade do |t|
     t.string "type"
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 20170814020801) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.date "shipping_date"
+    t.boolean "fulfilled"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "states", force: :cascade do |t|
