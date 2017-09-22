8cf14b4333a9716c99b47363b9337cc666d23d6b
Sean Luckett <whitecrane007@gmail.com>
2017-09-22 11:22:31 -0600

    Adds validations for User; association with Cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_one :cart
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user_attributes) do
+    {
+      first_name: 'Larry',
+      last_name: 'Jones',
+      email: 'tester@example.com'
+    }
+  end
+
+  it 'is valid with email, first_name, and last_name' do
+    user = User.new(user_attributes)
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without first_name' do
+    user = User.new(first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include "can't be blank"
+  end
+
+  it 'is invalid without last_name' do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include "can't be blank"
+  end
+
+  it 'is invalid without email' do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include "can't be blank"
+  end
+
+  it 'is invalid with duplicate email' do
+    User.create user_attributes
+    new_user = User.new(email: 'tester@example.com')
+    new_user.valid?
+    expect(new_user.errors[:email]).to include 'has already been taken'
+  end
+
+  it 'can have a cart' do
+    user = User.create user_attributes
+    expect(user.cart).to be_nil
+    user.create_cart
+    expect(user.cart.id).not_to be_nil
+  end
+end
