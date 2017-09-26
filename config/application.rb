2184ca6e1d3438369a0d7b6ea5c253806b850f1d
Sean Luckett <whitecrane007@gmail.com>
2017-09-26 13:33:56 -0600

    Adds FactoryGirl for fixtures

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,6 @@ module VikingStore
     # Don't generate system test files.
     config.generators do |g|
       g.test_framework :rspec,
-                       fixtures: false,
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: false
