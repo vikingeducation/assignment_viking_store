2ef1fff5eab39f753b99792057e7f3487190b451
Sean Luckett <whitecrane007@gmail.com>
2017-09-27 08:18:28 -0600

    Change CreditCard spec to use FactoryGirl

--- spec/factories/credit_cards.rb
+++ spec/factories/credit_cards.rb
@@ -5,6 +5,8 @@ FactoryGirl.define do
   factory :credit_card do
     number { Faker::Business.credit_card_number }
     expiration { Faker::Business.credit_card_expiry_date }
+
+    user
   end
 
 end
--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -3,32 +3,29 @@ require 'rails_helper'
 RSpec.describe CreditCard, type: :model do
 
   it 'is valid with user, number, and expiration date' do
-    card = CreditCard.new number: '123', expiration: '123'
-    card.build_user first_name: 'Joe', last_name: 'Smith',
-                    email: 'tester@example.com'
+    card = build :credit_card
     expect(card).to be_valid
   end
 
   it 'is valid without a cvs' do
-    card = CreditCard.new cvv: nil
-    card.valid?
-    expect(card.errors[:cvv]).not_to include "can't be blank"
+    card = build :credit_card, cvv: nil
+    expect(card).to be_valid
   end
 
   it 'is invalid without a user' do
-    card = CreditCard.new number: '123', expiration: '123'
+    card = build :credit_card, user: nil
     card.valid?
     expect(card.errors[:user]).to include 'must exist'
   end
 
   it 'is invalid without a number' do
-    card = CreditCard.new number: nil
+    card = build :credit_card, number: nil
     card.valid?
     expect(card.errors[:number]).to include "can't be blank"
   end
 
   it 'is invalid without an expiration' do
-    card = CreditCard.new expiration: nil
+    card = build :credit_card, expiration: nil
     card.valid?
     expect(card.errors[:expiration]).to include "can't be blank"
   end
