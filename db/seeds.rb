9c159eb20f82c518ed2cad6e06fbab5f2e988926
Sean Kelly <shonkelli@gmail.com>
2017-11-11 15:37:23 -0500

    began working on seeds file for addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,107 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ---
+# Constants
+# ---
+# STATES = [
+#   "Alabama",
+#   "Alaska",
+#   "Arizona",
+#   "Arkansas",
+#   "California",
+#   "Colorado",
+#   "Connecticut",
+#   "Delaware",
+#   "Florida",
+#   "Georgia",
+#   "Hawaii",
+#   "Idaho",
+#   "Illinois",
+#   "Indiana",
+#   "Iowa",
+#   "Kansas",
+#   "Kentucky",
+#   "Louisiana",
+#   "Maine",
+#   "Maryland",
+#   "Massachusetts",
+#   "Michigan",
+#   "Minnesota",
+#   "Mississippi",
+#   "Missouri",
+#   "Montana",
+#   "Nebraska",
+#   "Nevada",
+#   "New Hampshire",
+#   "New Jersey",
+#   "New Mexico",
+#   "New York",
+#   "North Carolina",
+#   "North Dakota",
+#   "Ohio",
+#   "Oklahoma",
+#   "Oregon",
+#   "Pennsylvania",
+#   "Rhode Island",
+#   "South Carolina",
+#   "South Dakota",
+#   "Tennessee",
+#   "Texas",
+#   "Utah",
+#   "Vermont",
+#   "Virginia",
+#   "Washington",
+#   "West Virginia",
+#   "Wisconsin",
+#   "Wyoming"
+# ]
+
+MULTIPLIER = 10
+
+
+# Remove existing data
+Address.delete_all
+User.delete_all
+
+# create_states
+STATES.each do |state_name|
+  State.create(:name => state_name)
+  num_states_created += 1
+  print_num_of(num_states_created, num_states)
+end
+
+# create_addresses
+MULTIPLIER.times do
+  Address.create(
+    line_1: Faker::Address.street_address,
+    line_2: Faker::Address.secondary_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip: Faker::Address.zip
+  )
+end
+# create_users
+MULTIPLIER.times do
+  mail_add, bill_add = Address.pluck(:id).sample(2)
+  # if [true, false].sample
+  #   bill_add = mail_add
+  # end
+  bill_add = mail_add if [true, false].sample
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.safe_email,
+    default_mail: mail_add,
+    default_bill: bill_add,
+    phone: Faker::PhoneNumber.phone_number
+  )
+end
+
+# create_categories
+MULTIPLIER.times do
+
+
+
+end
+
+
+# create_products
+
+  #
