70b702403fefcaad83994e6b9288a263de72d6f7
Sean Kelly <shonkelli@gmail.com>
2017-11-11 11:41:16 -0500

    added addresses table and attempted to create relationship with users

--- db/migrate/20171109033100_create_users.rb
+++ db/migrate/20171109033100_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :email
+      t.integer :default_mail
+      t.integer :default_bill
+      t.string :phone
       t.timestamps
     end
   end
---
+++ db/migrate/20171111154120_create_addresses.rb
@@ -0,0 +1,13 @@
+class CreateAddresses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :addresses do |t|
+      t.belongs_to :addresses
+      t.string :line_1, :null => false
+      t.string :line_2
+      t.string :city, :null => false
+      t.string :state, :null => false
+      t.string :zip, :null => false
+      t.timestamps
+    end
+  end
+end
