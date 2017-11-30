8fd3d1b7782f6dedaa022ddd749b85d5408b327a
anne <richardson.ae@gmail.com>
2017-11-30 09:30:38 -0600

    Adds address type

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+root 'address_types#index'
+resources :address_types
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
