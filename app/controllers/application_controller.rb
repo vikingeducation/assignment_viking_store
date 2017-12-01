d116c6f27a73e8f9f10632d691102077d23b2ca5
anne <richardson.ae@gmail.com>
2017-11-30 20:11:57 -0600

    WIP session and order cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_cart
+
+  def set_cart
+      @order = Order.find(session[:order_id])
+    rescue ActiveRecord::RecordNotFound
+      @order = Order.create
+      session[:order_id] = @order.id
+  end
+
 end
