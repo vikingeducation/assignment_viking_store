bd831b867e6121795e476675f03d087fcf254eae
anne <richardson.ae@gmail.com>
2017-11-30 19:20:33 -0600

    Adds session links to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def login_links
+    if current_user
+      link_to 'Sign Out', destroy_user_session_path
+    else
+      link_to 'Sign In', new_user_session_path
+    end
+  end
 end
